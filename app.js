af03f1193a256607b8089f32e08897ada01bb9e0
Timothy Lai <timothylai1121@gmail.com>
2023-06-03 17:10:56 -0400

    adding more work, noted, we have to have access to mongoDB, I am having difficult of testing

--- app.js
+++ app.js
@@ -1,30 +1,63 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const { graphqlHttp } = require("express-graphql");
-const mongoose = require("mongoose");
-
-const graphQlSchema = require('./graphql/schema/index');
-const graphQlResolvers = require('./graphql/resolvers/index');
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
 
 const app = express();
 
-const port = process.env.PORT || 3002;
+const events = [];
 
 app.use(bodyParser.json());
 
-
 app.use(
-  "/graphql",
+  '/graphql',
   graphqlHttp({
-    schema: graphQlSchema,
-    rootValue: graphQlResolvers,
+    schema: buildSchema(`
+        type Event {
+          _id: ID!
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+        input EventInput {
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+        type RootQuery {
+            events: [Event!]!
+        }
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return events;
+      },
+      createEvent: args => {
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date
+        };
+        events.push(event);
+        return event;
+      }
+    },
     graphiql: true
   })
 );
 
-mongoose
-  .connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@devapi.b0zoipm.mongodb.net/${process.env.MONGO_DB}`
-  )
-  .then(() => app.listen(port, () => console.log("server is running")))
-  .catch((err) => console.log(err));
+mongoose.connect('')
+
+app.listen(3000);
--- models/User.js
+++ models/User.js
@@ -1,25 +1,22 @@
-// adding User.js model first since it is the parent model //
-// make sure going back to app.js to add mongoose and connect to mongodb //
+const mongoose = require('mongoose');
 
-const mongoose = require("mongoose"); // import mongoose package
-const Schema = mongoose.Schema; // creating the schema
+const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({ // all string and required to enter username, email, and password //
-    email: {
-        type : String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    createdEvents: [
-        {
-        type: Schema.Types.ObjectId,
-        ref: 'Event' // this is important for mongoose to know that this is a relationship between the two models // // use ref Event models //
-        }
-    ]
-    
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdEvents: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Event'
+    }
+  ]
 });
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', userSchema);
