c90974755f097457ef9086379dd92f2394215f7e
Maha Abdelnabi <mahaomar@gmail.com>
2023-06-08 14:20:49 -0400

    add logout btn,create modal & bachdrps components

--- frontend/events-app/src/pages/Events.js
+++ frontend/events-app/src/pages/Events.js
@@ -1,12 +1,47 @@
 import React, { Component } from "react";
+import Modal from '../components/Modal/Modal';
+import Backdrop from '../components/Backdrop/Backdrop';
 import './Events.css';
 
 class EventsPage extends Component {
+  state = {
+    creating: false,
+  };
+
+  startCreateEventHandler = () => {
+    this.setState({ creating: true });
+  };
+
+  modalConfirmHandler = () => {
+    this.setState({ creating: false });
+  };
+
+  modalCancelHandler = () => {
+    this.setState({ creating: false });
+  };
   render() {
     return (
-      <div className="events-control">
-        <button className="btn">Create Event</button>
-      </div>
+     <>
+        {this.state.creating && <Backdrop />}
+        {this.state.creating && (
+          <Modal
+            title="Add Event"
+            canCancel
+            canConfirm
+            onCancel={this.modalCancelHandler}
+            onConfirm={this.modalConfirmHandler}
+          >
+            <p>Modal Content</p>
+          </Modal>
+        )}
+        <div className="events-control">
+          <p>Share your own Events!</p>
+          <button className="btn" onClick={this.startCreateEventHandler}>
+            Create Event
+          </button>
+        </div>
+      </>
+    
     );
   }
 }
