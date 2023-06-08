9fc5483d0017aaff2e2cc7ab488a8c00097534da
BrianLucy <116689598+BrianLucy@users.noreply.github.com>
2023-06-08 09:22:37 -0400

    Update Events.js

--- frontend/events-app/src/pages/Events.js
+++ frontend/events-app/src/pages/Events.js
@@ -4,9 +4,11 @@ import './Events.css';
 class EventsPage extends Component {
   render() {
     return (
-      <div className="events-control">
-        <button className="btn">Create Event</button>
-      </div>
+      <h1>Time to create some magic</h1>
+    <h3>click the wand to begin</h3>
+  <div className="events-control">
+        <button className="btn"><img src="/transparent wand.png"></button>
+  </div>
     );
   }
 }
